3e513afd0d7844522d785e970bcb453c9ae8336c
GCose <goodnessadewuyi8@gmail.com>
2026-04-26 15:46:19 +0000

    update/updated-file-service-ecosystem

--- utils/url.ts
+++ utils/url.ts
@@ -1,2 +1,2 @@
 export const BASE_URL: string = 'https://soshsa-backend-yl14.onrender.com/api';
-export const JEETIX_BASE_URL: string = 'https://jeetix-file-service.onrender.com';
+export const JEETIX_BASE_URL: string = 'https://file-service-1t33.onrender.com';
